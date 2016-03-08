622a29cdaa662fd9de50958931a67429bb4cbfc2
Andy Smith <asmith@mapaction.org>
2016-03-08 00:05:07 +0000

    using some list comprehensions

--- check_wpkg_logs.py
+++ check_wpkg_logs.py
@@ -10,11 +10,17 @@ def get_log_files(logs_dir, recursive=False):
                 dirs.pop()
 
         # only include relevant files
-        include_files = 'wpkg.*log$' # for files only
+        include_files = r'wpkg.*log$' # for files only
         files[:] = [os.path.join(root, f) for f in files]
         files[:] = [f for f in files if re.search(include_files, f)]
 
         return files
+
+def get_file_contents(l_file):
+    f = open(l_file)
+    txt = f.read()
+    f.close()
+    return txt
 
 def check_wpkg_stable_branch(log_text):
     return re.search(r"sysadmin/stable/installers/wpkg/hosts.xml", log_text, re.IGNORECASE) is not None
@@ -31,21 +37,39 @@ if __name__ == '__main__':
 
     # files[:] = [f for f in log_files if re.search(include_files, f)]
 
-    for log_file in log_files:
-        # print(log_file)
-        f = open(log_file)
-        log_file_text = f.read()
-        # print(log_file_text)
-        # print("stable={},  name={}".format(check_wpkg_stable_branch(log_file_text), log_file))
+    # f_array = [(f for f in log_files if get_file_contents(f)]
+    f_array = [(f, get_file_contents(f)) for f in log_files]
+    print ("initial file count = {}".format(len(f_array)))
+    # print (f_array[0][1])
+    f_array[:] = [f for f in f_array if check_wpkg_stable_branch(f[1])]
+    print ("stable log file count = {}".format(len(f_array)))
 
-        # files[:] = [f for f in log_files if re.search(include_files, f)]
+    err_array = [(f[0], err_msg) for f in f_array for err_msg in get_wpkg_errors(f[1])]
+
+    for e in err_array:
+        print ("{}\t{}".format(e[0], e[1]))
+
+    print ("err msg count = {}".format(len(err_array)))
 
 
-        if check_wpkg_stable_branch(log_file_text):
-            err_msgs = get_wpkg_errors(log_file_text)
+    # files_details = [(f, get_file_contents(f)) for f in log_files]
+    # print(files_details[1])
 
-            if len(err_msgs) > 0:
-                print("Errors in logfile {}".format(log_file))
 
-                for err_msg in err_msgs:
-                    print(err_msg)
+    # for log_file in log_files:
+    #     # print(log_file)
+    #     # f = open(log_file)
+    #     # log_file_text = f.read()
+    #     log_file_text = get_file_contents(log_file)
+    #     # print(log_file_text)
+    #     # print("stable={},  name={}".format(check_wpkg_stable_branch(log_file_text), log_file))
+    #
+    #
+    #     if check_wpkg_stable_branch(log_file_text):
+    #         err_msgs = get_wpkg_errors(log_file_text)
+    #
+    #         if len(err_msgs) > 0:
+    #             print("Errors in logfile {}".format(log_file))
+    #
+    #             for err_msg in err_msgs:
+    #                 print(err_msg)
--- test_check_wpkg_logs.py
+++ test_check_wpkg_logs.py
@@ -11,3 +11,6 @@ class TestCheckWPKGlogs(unittest.TestCase):
     def tearDown(self):
         pass
 
+    def test_check_wpkg_stable_branch(self):
+        # check_wpkg_stable_branch
+        pass
